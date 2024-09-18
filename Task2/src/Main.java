2747d1fb85a4119bec9e52f23ba8cfea8dd50980
Whiteburn2023 <erunda995@yandex.ru>
2024-09-18 14:41:31 +0500

    ex1 ready

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,6 +1,14 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
+    private static final String text = "My Wonderful Family. I live in a house near the mountains. I have two brothers and one sister, and I was born last. My father teaches mathematics, and my mother is a nurse at a big hospital. My brothers are very smart and work hard in school. My sister is a nervous girl, but she is very kind. My grandmother also lives with us. She came from Italy when I was two years old. She has grown old, but she is still very strong. She cooks the best food! My family is very important to me. We do lots of things together. My brothers and I like to go on long walks in the mountains. My sister likes to cook with my grandmother. On the weekends we all play board games together. We laugh and always have a good time. I love my family very much. ";
     public static void main(String[] args) {
-        //todo  код
+        String input = text.replaceAll("[.,!:;]", "");
+        String[] arr = input.split(" ");
+        for (String name : arr) {
+            System.out.println(name);
+        }
     }
 
     //todo метод
