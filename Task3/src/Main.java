de3e8d2cf4df146f751b2730e2f9398c390f835f
Whiteburn2023 <erunda995@yandex.ru>
2024-09-18 15:45:10 +0500

    ex2 ready. test OK

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+import java.util.SplittableRandom;
+
 public class Main {
     public static void main(String[] args) {
+        int spaceCount = 0;
+        System.out.println("Введите ФИО");
+        String input = new Scanner(System.in).nextLine().trim();
+        for (int i = 0; i < input.length(); i++){
+            if (input.charAt(i) == ' '){
+            spaceCount++;
+            }
+        }
+        if (input.matches("[А-я\\s-]+") && spaceCount == 2) {  //
+
+            String[] arr = input.trim().split(" ");
+            System.out.println("Фамилия: " + arr[0]);
+            System.out.println("Имя: " + arr[1]);
+            System.out.println("Отчество: " + arr[2]);
+        } else {
+            System.out.println("Введенная строка не является ФИО");
+        }
+
+
         //todo ФИО
     }
 }
