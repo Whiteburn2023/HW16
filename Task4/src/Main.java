1df7d8539c2a39e06db29f5fa2238c0e47d55063
Whiteburn2023 <erunda995@yandex.ru>
2024-09-18 22:40:00 +0500

    ex3 ready. test OK

--- Task4/src/Main.java
+++ Task4/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //todo Numbers
+        while (true) {
+            System.out.println("Введите телефонный номер: ");
+            String phone = new Scanner(System.in).nextLine().trim();
+            phone = phone.replaceAll("[^\\d]", "");
+            if (phone.matches("^[78][0-9]{10}$")) {
+                System.out.println(phone);
+            } else {
+                System.out.println("Неверный формат номера");
+            }
+        }
     }
 }
